304cd4284325bc0a1a70bd0a2a1892423d8dad76
Yu-Wei Tseng <dadavidtseng@gmail.com>
2026-02-15 13:31:21 -0600

    fix(ui): correct vertex winding order in health icon rendering
    Swap vertices 1 and 2 in UIHandler::InitializePlayerShipLocalVerts()
    to match PlayerShip::InitializeLocalVerts() winding order. The reversed
    triangle caused the top crown to render with incorrect face direction.

--- Code/Game/UIHandler.cpp
+++ Code/Game/UIHandler.cpp
@@ -284,8 +284,8 @@ void UIHandler::DrawPlayerShip(Vec2 const& drawPosition, Rgba8 const& color, flo
 void UIHandler::InitializePlayerShipLocalVerts()
 {
     m_localVerts[0].m_position = Vec3(-2.f, 1.f, 0.f);
-    m_localVerts[1].m_position = Vec3(0.f, 2.f, 0.f);
-    m_localVerts[2].m_position = Vec3(2.f, 1.f, 0.f);
+    m_localVerts[1].m_position = Vec3(2.f, 1.f, 0.f);
+    m_localVerts[2].m_position = Vec3(0.f, 2.f, 0.f);
 
     m_localVerts[3].m_position = Vec3(0.f, 1.f, 0.f);
     m_localVerts[4].m_position = Vec3(-2.f, 1.f, 0.f);
